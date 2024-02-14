9c70c38fd4bed8d3b464fd66fc994e5bce163d6f
Aniket Samanta <aniket.samanta@coxautoinc.com>
2024-02-14 20:24:37 +0530

    fix prod deploy

--- projects/container-app/webpack.prod.config.js
+++ projects/container-app/webpack.prod.config.js
@@ -2,12 +2,11 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "insuranceMfeApp": "https://localhost:8080/insurance-mfe-app/remoteEntry.js",
-    "premiumMfeApp": "https://localhost:8080/premium-mfe-app/remoteEntry.js",
+    "insuranceMfeApp": "https://client-side-mfe-architecture-nagp.netlify.app/insurance-mfe-app/remoteEntry.js",
+    "premiumMfeApp": " https://client-side-mfe-architecture-nagp.netlify.app/premium-mfe-app/remoteEntry.js",
   },
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
-
 });
